1978f17938f1ea601dfe9dfe5d1bd7adfbc5c351
Batishchev Pavel <batishchev.pavel@gmail.com>
2014-12-04 23:16:18 +0000

    говняга

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-//jQuery('document').ready(function($){
-//   var menu = $('.top-menu');
-//
-//   $(window).scroll(function(){
-//      if($(this).scrollTop() > 10){
-//         menu.addClass('top-menu-fixed');
-//      } else {
-//         menu.removeClass('top-menu-fixed');
-//      }
-//   });
-//});
+jQuery('document').ready(function($){
+   var menu = $('.top-menu');
+
+   $(window).scroll(function(){
+      if($(this).scrollTop() > 806){
+         //console.log($(this).scrollTop());
+         menu.addClass('top-menu-fixed');
+      } else {
+         menu.removeClass('top-menu-fixed');
+      }
+   });
+});
