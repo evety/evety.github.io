53cffa96cc3cfb305473679a7c23be60ac65e08b
Batishchev Pavel <batishchev.pavel@gmail.com>
2014-12-04 23:37:00 +0000

    убрал js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-jQuery('document').ready(function($){
-   var menu = $('.top-menu');
-
-   $(window).scroll(function(){
-      if($(this).scrollTop() > 806){
-         //console.log($(this).scrollTop());
-         menu.addClass('top-menu-fixed');
-      } else {
-         menu.removeClass('top-menu-fixed');
-      }
-   });
-});
+//jQuery('document').ready(function($){
+//   var menu = $('.top-menu');
+//
+//   $(window).scroll(function(){
+//      if($(this).scrollTop() > 806){
+//         //console.log($(this).scrollTop());
+//         menu.addClass('top-menu-fixed');
+//      } else {
+//         menu.removeClass('top-menu-fixed');
+//      }
+//   });
+//});
