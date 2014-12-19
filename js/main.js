e6bce5fee5ae2b0031ac89fe1d14ea7c059f1f9d
Batishchev Pavel <batishchev.pavel@gmail.com>
2014-12-19 22:51:44 +0000

    анимация landing-page в предложениях

--- js/main.js
+++ js/main.js
@@ -19,3 +19,17 @@ $.stellar({
 //}, function() {
 //   $('.tooltip').css('-webkit-transform', 'translate(0, 0)');
 //});
+
+jQuery(document).ready(function($) {
+   var slide4 = $('.fourth-slide .first');
+   var image = slide4.find('.image');
+   var sliding = slide4.find('.sliding');
+
+   slide4.mouseover(function() {
+      image.css('background', 'url("/images/landing2.png") no-repeat center center');
+      sliding.css('margin-left', 'calc(-100% - 30px)');
+   }).mouseout(function() {
+      image.css('background', 'url("/images/landing.png") no-repeat center center');
+      sliding.css('margin', '0');
+   });
+});
