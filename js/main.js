a087679e157c143161828ae3ff961ed47d24b61a
Batishchev Pavel <batishchev.pavel@gmail.com>
2015-01-07 16:26:15 +0000

    3d

--- js/main.js
+++ js/main.js
@@ -1,35 +1,65 @@
-//jQuery('document').ready(function($){
-//   var menu = $('.top-menu');
-//
-//   $(window).scroll(function(){
-//      if($(this).scrollTop() > 806){
-//         //console.log($(this).scrollTop());
-//         menu.addClass('top-menu-fixed');
-//      } else {
-//         menu.removeClass('top-menu-fixed');
-//      }
-//   });
-//});
-$.stellar({
-   horizontalOffset: 50
+var element = document.getElementById('works-screen');
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, element.offsetWidth / element.offsetHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(element.offsetWidth, element.offsetHeight);
+element.appendChild(renderer.domElement);
+
+var h = 800;
+var w = 2241;
+var r = w / (2 * 3.14);
+
+r = r / 10;
+h = h / 10;
+
+console.log(r);
+console.log(h);
+
+var geometry = new THREE.CylinderGeometry(r, r, h, 32, 1, true);
+
+//var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+
+geometry.applyMatrix( new THREE.Matrix4().makeScale( -1, 1, 1 ) );
+var material = new THREE.MeshBasicMaterial({
+   map: THREE.ImageUtils.loadTexture('/images/screen25.png')
 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-//$('.fourth-slide .inner').hover(function(){
-//   $('.tooltip').css('-webkit-transform', 'translate(-600px, 0)');
-//}, function() {
-//   $('.tooltip').css('-webkit-transform', 'translate(0, 0)');
-//});
+camera.position.z = 10;
 
-jQuery(document).ready(function($) {
-   var slide4 = $('.fourth-slide .first');
-   var image = slide4.find('.image');
-   var sliding = slide4.find('.sliding');
+function render() {
+   requestAnimationFrame(render);
 
-   slide4.mouseenter(function() {
-      image.css('margin-left', 'calc(-100% - 30px)');
-      sliding.css('margin-left', 'calc(-100% - 30px)');
-   }).mouseleave(function() {
-      image.css('margin', '0');
-      sliding.css('margin', '0');
-   });
-});
+   //cube.rotation.x += 0.01;
+   //cube.rotation.y += 0.01;
+
+   renderer.render(scene, camera);
+}
+
+render();
+
+//document.addEventListener( 'mousewheel', onDocumentMouseWheel, false );
+//document.addEventListener( 'DOMMouseScroll', onDocumentMouseWheel, false);
+
+
+/*
+window.onscroll = onDocumentMouseWheel;
+
+var y = window.pageYOffset;
+
+function onDocumentMouseWheel(event) {
+   var value = window.pageYOffset - y;
+
+   console.log(value);
+
+   cube.position.y += value * 0.007;
+
+   y = window.pageYOffset;
+
+				//camera.updateProjectionMatrix();
+}
+
+*/
